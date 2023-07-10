0e3f118157ec69f92f7e3cb70b5c9d663e89df4e
Arthur Batista Furlan <arthurbfx2001@gmail.com>
2023-07-10 12:26:48 -0300

    add sell adm

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
 const app = express();
 const path = require("path");
 
@@ -9,30 +8,6 @@ app.use(express.json());
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
-});
-
-app.get("/gerar-pdf", async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("http://localhost:8081", { waitUntil: "networkidle0" });
-
-  const width = 900;
-  const height = 400;
-  await page.setViewport({ width, height });
-
-  // Gera o PDF
-  const pdfBuffer = await page.pdf({ format: "A4", width, height });
-
-  await browser.close();
-
-  // Define o cabeçalho para forçar o download do arquivo
-  res.set({
-    "Content-Type": "application/pdf",
-    "Content-Disposition": 'attachment; filename="formulario-de-pagamento.pdf"',
-  });
-
-  // Envie o buffer do PDF como resposta
-  res.send(pdfBuffer);
 });
 
 app.listen(8081, function () {
