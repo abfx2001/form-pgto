b4a344b973883d3cda035fcb948a7eee275a771e
Arthur Batista Furlan <arthurbfx2001@gmail.com>
2023-07-10 12:29:10 -0300

    troca de porta

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get("/", function (req, res) {
 });
 
 app.listen(8081, function () {
-  console.log("Servidor aberto na porta 8081");
-  console.log("http://localhost:8081");
+  console.log("Servidor aberto na porta 8082");
+  console.log("http://localhost:8082");
 });
