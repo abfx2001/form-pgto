e432def2c5b89a8fe81db96d4ce3c47cf2c6f685
Arthur Batista Furlan <arthurbfx2001@gmail.com>
2023-07-18 16:16:35 -0300

    juste

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get("/", function (req, res) {
 
 app.listen(8081, function () {
   console.log("Servidor aberto na porta 8082");
-  console.log("http://localhost:8082");
+  console.log("http://localhost:8081");
 });
